37fc49a1f4ed73c5b5d02142281f6998458ab915
PaoloGiraudi <58827439+PaoloGiraudi@users.noreply.github.com>
2021-03-11 11:15:41 +0100

    added Skeleton to timeline

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,5 +1,15 @@
 import React from "react";
+import Skeleton from "react-loading-skeleton";
 
 export default function Timeline() {
-  return <p>I am the timeline</p>;
+  const photos = null;
+  return (
+    <div className="container col-span-2">
+      {!photos ? (
+        <Skeleton count={4} width={640} height={500} className="mb-5" />
+      ) : (
+        photos.map((content) => <p>I will be the photo</p>)
+      )}
+    </div>
+  );
 }
