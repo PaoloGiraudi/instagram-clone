77ec46616708ff3306cd91ef99571dde78248dc6
PaoloGiraudi <58827439+PaoloGiraudi@users.noreply.github.com>
2021-03-23 15:00:34 +0100

    first draft post added in timeline

---
+++ src/components/post/index.js
@@ -0,0 +1,13 @@
+import Actions from "./actions";
+import Image from "./image";
+import Header from "./header";
+import Footer from "./footer";
+import Comments from "./comments";
+
+export default function Post({ content }) {
+  return (
+    <div className="rounded col-span-4 border bg-white mb-16">
+      <p>Hej I am a post</p>
+    </div>
+  );
+}
