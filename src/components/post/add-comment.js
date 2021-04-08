0fd8c14bf6194b219a63dc6af0c1249d0c536046
PaoloGiraudi <58827439+PaoloGiraudi@users.noreply.github.com>
2021-04-08 15:00:36 +0200

    add comments works

--- src/components/post/add-comment.js
+++ src/components/post/add-comment.js
@@ -1,5 +1,66 @@
 import React, { useState, useContext } from "react";
+import FirebaseContext from "../../context/firebase";
+import UserContext from "../../context/user";
 
-export default function AddComment({}) {
-  return <div>Hej</div>;
+export default function AddComment({
+  docId,
+  comments,
+  setComments,
+  commentInput,
+}) {
+  const [comment, setComment] = useState("");
+  const { firebase, FieldValue } = useContext(FirebaseContext);
+  const {
+    user: { displayName },
+  } = useContext(UserContext);
+
+  const handleSubmitComment = (event) => {
+    event.preventDefault();
+
+    setComments([{ displayName, comment }, ...comments]);
+    setComment("");
+
+    return firebase
+      .firestore()
+      .collection("photos")
+      .doc(docId)
+      .update({
+        comments: FieldValue.arrayUnion({ displayName, comment }),
+      });
+  };
+  return (
+    <div className="border-t border-gray">
+      <form
+        className="flex w-full justify-between pl-0 pr-5"
+        onSubmit={(event) =>
+          comment.length >= 3
+            ? handleSubmitComment(event)
+            : event.preventDefault()
+        }
+        method="POST"
+      >
+        <input
+          type="text"
+          aria-label="Add a Comment"
+          autoComplete="off"
+          className="text-sm text-gray w-full mr-3 py-5 px-4"
+          name="add-comment"
+          placeholder="Add a Comment..."
+          value={comment}
+          onChange={({ target }) => setComment(target.value)}
+          ref={commentInput}
+        />
+        <button
+          className={`text-sm font-bold text-blue-500 ${
+            !comment && "opacity-25"
+          }`}
+          type="button"
+          disabled={comment.length < 3}
+          onClick={handleSubmitComment}
+        >
+          Post
+        </button>
+      </form>
+    </div>
+  );
 }
--- src/components/post/header.js
+++ src/components/post/header.js
@@ -10,7 +10,7 @@ export default function Header({ username }) {
           <img
             src={`images/avatars/${username}.jpg`}
             className="rounded-full h-8 w-8 flex mr-3"
-            alt={`${username} profile picture`}
+            alt={`${username} profile pic`}
           />
           <p className="font-bold">{username}</p>
         </Link>
