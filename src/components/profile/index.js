778fa6e87bd73684d63428fe26d17528391acc55
PaoloGiraudi <58827439+PaoloGiraudi@users.noreply.github.com>
2021-04-17 16:16:31 +0200

    getting users photos and followers

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,11 +1,45 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useReducer } from "react";
+import {
+  getUserByUsername,
+  getUserPhotosByUsername,
+} from "../../services/firebase";
 import Header from "./header";
 import Photos from "./photos";
 
+const reducer = (state, newState) => ({ ...state, ...newState });
+const initialState = {
+  profile: {},
+  photosCollection: [],
+  followerCount: 0,
+};
+
 export default function Profile({ username }) {
+  const [{ profile, photosCollection, followerCount }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  useEffect(() => {
+    async function getProfileInfoAndProfile() {
+      const [{ ...user }] = await getUserByUsername(username);
+      const photos = await getUserPhotosByUsername(username);
+
+      dispatch({
+        profile: user,
+        photosCollection: photos,
+        followerCount: user.followers.length,
+      });
+    }
+    getProfileInfoAndProfile();
+  }, [username]);
   return (
     <>
-      <Header />
+      <Header
+        photosCollection={photosCollection.length}
+        profile={profile}
+        followerCount={followerCount}
+        setFollowerCount={dispatch}
+      />
       <Photos />
     </>
   );
