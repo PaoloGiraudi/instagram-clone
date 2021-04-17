7c9b64a32507816269ba748487ccbc4001ca741e
PaoloGiraudi <58827439+PaoloGiraudi@users.noreply.github.com>
2021-04-17 15:50:41 +0200

    created profile component and getUserByUsername function

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import { getUserByUsername } from "../services/firebase";
+import Header from "../components/header.js";
+import UserProfile from "../components/profile";
+import * as ROUTES from "../constans/routes";
 
 export default function Profile() {
-  return <p>Hello from Profile</p>;
+  const { username } = useParams();
+  const [userExists, setUserExist] = useState(undefined);
+  const history = useHistory();
+
+  useEffect(() => {
+    async function checkUserExistsToLoadProfile() {
+      const doesUserExist = await getUserByUsername(username);
+      if (!doesUserExist) {
+        history.push(ROUTES.NOT_FOUND);
+      } else {
+        setUserExist(true);
+      }
+    }
+    checkUserExistsToLoadProfile();
+  }, [username, history]);
+
+  return userExists ? (
+    <div className="bg-gray">
+      <Header />
+      <div className="mx-auto max-w-screen-lg">
+        <UserProfile username={username} />
+      </div>
+    </div>
+  ) : null;
 }
